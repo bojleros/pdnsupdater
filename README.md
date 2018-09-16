8f9ee34a4a8c208c348bf62098c0b6498b5b84b6
Bartek Rekke <bojleros@gmail.com>
2018-09-16 17:01:44 +0200

    Major code refactor

--- README.md
+++ README.md
@@ -1,43 +1,56 @@
 # pdnsupdater
 Simple Flask API Gateway for updating pDNS database records
 
+### Description
+This app can be used for updating existing A records in a quite simple way:
 
-This app can be used for updating existing A records like that:
+```
+curl -k -v -H "Content-Type: application/json" -X POST -d '{"user":"username", "pswd":"yourpass", "fqdn" : "mail.example.net", "value":"1.1.1.1"}'  https://localhost:8888/update
+```
 
-curl -k -v -H "Content-Type: application/json" -X POST -d '{"user":"username", "pswd":"yourpass", "value":"1.1.1.1"}'  https://localhost:8888/update
+Pdnsupdater connects DB backend and directly alters records. You can even ommit some parameters so client ip address will be used for updating user's first record:
 
-Parameters like value and fqdn are optional. If ommited app will default to request source address and to the first entitled fqdn.
+```
+curl -k -v -H "Content-Type: application/json" -X POST -d '{"user":"username", "pswd":"yourpass"}'  https://localhost:8888/update
+```
 
 
-Dependencies:
+### Dependencies
+
+```
 flask
 mysql-connector-python
+```
 
 
-Starting application:
+### Starting application
 
-!! First prepare configuration /etc/pdnsupdater/config.json
+First generate password hashes for your users. You can use interactive python shell but don't forget to import hashlib !
+Second prepare configuration /etc/pdnsupdater/config.json or ~/config.json
 
-Starting for development:
+### Starting for development
 python pdnsupdater
 
 !! Note this starts builtin wsgi server without SSL. Anyone can intercept your password ...
 
 
-More reasonable way of starting:
--use gunicorn with built-in ssl
+### More reasonable way of starting
+
+#### Use gunicorn with built-in ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $(hostname -s).key -out $(hostname -s).crt
 gunicorn -b 0.0.0.0:8443 -w 2 --keyfile $(hostname -s).key --certfile $(hostname -s).crt pdnsupdater:app
 
 
--use nginx+gunicorn
--use apache+wsgi+gunicorn
+#### Use nginx+gunicorn
 
+TBD.
 
+#### Use apache+wsgi+gunicorn
 
-Todos:
+TBD.
 
-Refactor/Cleanup
-Modular construction that allows different db's (at least Postgresql)
+###Todos
+
+Modular or semi-modular construction that allows different db's (at least Postgresql)
 More reasonable input validation ;)
--- pdnsupdater.py
+++ pdnsupdater.py
@@ -8,6 +8,7 @@ import socket
 import time
 import hashlib
 
+
 from flask import Flask
 from flask import request
 import logging
@@ -16,204 +17,220 @@ import mysql.connector as mariadb
 from mysql.connector import errorcode
 
 app = Flask(__name__)
-app.debug = True
 
-opts=list()
+cfg=None
+
+class UpdateNotNeeded(Exception):
+  '''
+  Custom exception
+  '''
+  def __init__(self,*args,**kwargs):
+    Exception.__init__(self,*args,**kwargs)
+
+class RecordUpdated(Exception):
+  '''
+  Custom exception
+  '''
+  def __init__(self,*args,**kwargs):
+    Exception.__init__(self,*args,**kwargs)
+
+
+class Db:
+  '''
+  This class is responsible for handling sql operations
+  '''
+  def __init__(self,cfg):
+    self.conn=None
+    self.cur=None
+
+    dbconf = cfg.get('mariadb',None)
+    if dbconf != None:
+      try:
+        dbconf['connection_timeout'] = float(dbconf['connection_timeout'])
+        self.conn = mariadb.connect(**dbconf)
+        self.cur = self.conn.cursor()
+      except Exception as e:
+        raise e
+
+    dbconf = cfg.get('postgres',None)
+    if dbconf != None:
+      raise NotImplementedError("Postgresql is not supported yet !")
+
+  def __del__(self):
+    self.conn.close()
+
+  def soa_bumpup(self,name):
+    '''
+    This method bumps up SOA serial every time we change record
+    It also preserves SN format avoidint overlaps
+    '''
+    domain='.'.join(name.strip().split('.')[1:])
+
+    try:
+      self.cur.execute( "SELECT id,content FROM records WHERE type='SOA' AND name='%s'" % (domain), )
+    except Exception as e:
+      self.__del__()
+      raise e
+
+    out = self.cur.fetchall()
+    if len(out) != 1:
+      self.__del__()
+      raise ValueError("More than one or zero 'SOA' records matched")
+
+    id=out[0][0]
+    soa=out[0][1].strip()
+    soa_array=soa.split()
+
+    #soa woodoo begins here
+    domainsn=int(soa_array[2])
+    sn = domainsn % 100
+    dat = str(domainsn / 100)
+    now = time.strftime("%Y%m%d")
+
+    if dat == now:
+      sn +=1
+      if sn >99:
+        sn=0
+      newsn = "%s%02d" % (dat,sn)
+    else:
+      newsn = now + "00"
+
+    soa_array[2] = str(newsn)
+    soa = ' '.join(soa_array)
+
+    try:
+      self.cur.execute( "UPDATE records SET content = '%s' WHERE type='SOA' and id=%d" % (str(soa),id) ,  )
+    except Exception as e:
+      self.__del__()
+      raise e
+
+    raise RecordUpdated("")
+
+  def update_A(self,name,value):
+    '''
+    This one updates 'A' records and also triggers soa_bumpup. First step is IP addr validation
+    '''
+
+    try:
+      socket.inet_aton(value)
+    except socket.error:
+      raise Exception("This is not an ip address !")
+
+    try:
+      self.cur.execute("SELECT content FROM records where type='A' and name='%s'" % (str(name)),)
+    except Exception as e:
+      self.__del__()
+      raise e
+
+    out = self.cur.fetchall()
+    if len(out) != 1:
+      self.__del__()
+      raise ValueError("More than one or zero 'A' records matched")
+
+    current_value = str(out[0][0]).strip()
+
+    if current_value != value:
+      try:
+        self.cur.execute( "UPDATE records SET content = '%s' WHERE type='A' AND name='%s'" % (str(value),str(name)) ,  )
+      except Exception as e:
+        self.__del__()
+        raise e
+      try:
+        self.soa_bumpup(name)
+      except RecordUpdated as e:
+        pass
+      except Exception as e:
+        self.__del__()
+        raise e
+      self.conn.commit()
+      raise RecordUpdated("Record updated")
+    else:
+      raise UpdateNotNeeded("No update needed")
+
+
+
 
 @app.route('/update', methods=['POST'])
 def update():
-	global opts
-	conf, lh = opts
+  global cfg
 
-	req = request.get_json()
-	user = req.get('user', None)
-	pswd = req.get('pswd', None)
-	fqdn = req.get('fqdn', None)
-	value = req.get('value', None)
+  req = request.get_json()
+  user = req.get('user', None)
+  pswd = req.get('pswd', None)
+  fqdn = req.get('fqdn', None)
+  value = req.get('value', None)
 
-	#we need input validation here and now
+  if user == None or pswd == None:
+    #please provide this two variables via post
+    return "Invalid creds", 403
 
-	if user == None or pswd == None:
-		#please provide this two variables via post
-		lh.warning("%s@%s Invalid creds" % ("unknown",str(request.remote_addr)) )
-		return "Invalid creds", 403
-	
-	userdef = conf['creds'].get(user, None)
-	pswd = hashlib.sha512(pswd).hexdigest()
-	if userdef == None:
-		#nonexistient user
-		lh.warning("%s@%s No such user" % ("unknown",str(request.remote_addr)) )
-		return "Invalid creds", 403
-	
-	if userdef['pass'] != pswd:
-		#incorrect password
-		lh.warning("%s@%s Incorrect password" % (str(user),str(request.remote_addr)) )
-		return "Invalid creds", 403
-	
-	lh.warning("%s@%s Correct credentials" % (str(user),str(request.remote_addr)) )
-		
-	#request lacking a parameter fqdn will alter first record on a list
-	if fqdn != None:
-		if fqdn not in userdef['records']:
-			return "You are not allowed to tackle with this record", 400
-	else:
-		fqdn = conf['creds'][user]['records'][0]
+  userdef = cfg['creds'].get(user, None)
+  pswd = hashlib.sha512(pswd).hexdigest()
+  if userdef == None:
+    #nonexistient user
+    return "Invalid creds", 403
 
-	domain = '.'.join(fqdn.split('.')[1:])
-	hostn = fqdn[0]
+  if userdef['pass'] != pswd:
+    #incorrect password
+    return "Invalid creds", 403
 
-	#request with no value field will update record with a peer address
-	if value != None:	
-		try:
-			socket.inet_aton(value)
-		except:
-			return "Invalid IP address", 400
-	else:
-		value = request.remote_addr
-	
-	value=str(value)
+  #request lacking a parameter fqdn will alter first record on a list
+  if fqdn != None:
+    if fqdn not in userdef['records']:
+  	   return "You are not allowed to tackle with this record", 400
+  else:
+    fqdn = cfg['creds'][user]['records'][0]
 
+  domain = '.'.join(fqdn.split('.')[1:])
+  hostn = fqdn[0]
 
-	#mariadb.connect() accepts dictionary of parameters :)
-	dbconf=conf.get('mariadb',None)
-	if dbconf==None:
-		lh.error("No database config under mariadb key")
-        	return "Database connection error", 500
-	dbconf['connection_timeout'] = float(dbconf['connection_timeout'])
-	
-	try:
-		conn = mariadb.connect(**dbconf)
-	except Exception as e:
-		lh.error("Unable to connect database : {}".format(str(e)))
-		return "Database connection error", 500
+  #request with no value field will update record with a peer address
+  if value != None:
+    try:
+  	  socket.inet_aton(value)
+    except:
+  	  return "Invalid IP address", 400
+  else:
+    value = request.remote_addr
 
-	try:
-		cur = conn.cursor()
-	except Exception as e:
-		conn.close()
-        	lh.error("Unable to open cursor : {}".format(str(e)))
-                return "Database cursor opening error", 500
+  value=str(value)
+
+  d=Db(cfg)
+  try:
+    d.update_A(fqdn,value)
+  except RecordUpdated as e:
+    del d
+    return "Updated", 200
+  except UpdateNotNeeded as e:
+    del d
+    return "Update not needed", 201
+  except Exception as e:
+    return str(e), 500
 
 
-	#get domainid
-	try:
-		cur.execute( "SELECT id FROM domains WHERE name = '%s'" % (str(domain)) ,)
-	except Exception as e:
-		conn.close()
-                lh.error("SQL Error : {}".format(str(e)))
-                return "SQL Error", 500
-		
-	out = cur.fetchall()
-	if len(out) != 1:
-		conn.close()
-                lh.error("Domain matching error : {}".format(str(e)))
-                return "Data error", 500
-	
-	domain_id = out[0][0]
-	
-	#get current ip for this record
-	try:
-                cur.execute( "SELECT content FROM records WHERE type='A' AND name='%s' and domain_id=%d" % (str(fqdn),domain_id) , )
-        except Exception as e:
-                conn.close()
-                lh.error("SQL Error : {}".format(str(e)))
-                return "SQL Error", 500
 
-	out = cur.fetchall()
-	if len(out) != 1:
-		conn.close()
-                lh.error("Record A matching error : {}".format(str(e)))
-                return "Data error", 500
-
-	cur_ip_a = str(out[0][0])
-
-	if cur_ip_a != value:
-		#Ip is different , we must update
-		try:
-			cur.execute( "UPDATE records SET content = '%s' WHERE type='A' AND name='%s' and domain_id=%d" % (str(value),str(fqdn),domain_id) ,  )
-		except Exception as e:
-			conn.close()
-	                lh.error("Record A update error : {}".format(str(e)))
-        	        return "Update error", 500
-		try:
-			cur.execute( "SELECT content FROM records WHERE type='SOA' AND domain_id=%d" % (domain_id), )
-		except Exception as e:
-                        conn.close()
-                        lh.error("Get SOA content error : {}".format(str(e)))
-                        return "Update error", 500
-		
-		out = cur.fetchall()
-		if len(out) != 1:
-                        conn.close()
-                        lh.error("More than one SOA !?!?! : {}".format(str(e)))
-                        return "Update error", 500
-		
-		#it's time for SOA arithmetics adventure
-		soa = str(out[0][0]).strip()
-		soa_array = soa.split()
-
-		domainsn=int(soa_array[2])
-	        sn = domainsn % 100
-        	dat = str(domainsn / 100)
-	        now = time.strftime("%Y%m%d")
-
-        	if dat == now:
-                	sn +=1
-                	if sn >99:
-                        	sn=0
-                	newsn = "%s%02d" % (dat,sn)
-	        else:
-        	        newsn = now + "00"
-		
-		soa_array[2] = str(newsn)
-		soa = ' '.join(soa_array)
-		
-		try:
-			cur.execute( "UPDATE records SET content = '%s' WHERE type='SOA' and domain_id=%d" % (str(soa),domain_id) ,  )
-		except Exception as e:
-			conn.close()
-                        lh.error("SOA update error : {}".format(str(e)))
-                        return "Update error", 500
-
-		lh.warning("%s@%s Updated %s => %s" % (str(user),str(request.remote_addr),fqdn,str(value),) )
-		conn.commit()
-	        cur.close()
-        	conn.close()
-
-
-	        return "Updated",200
-
-
-	lh.warning("%s@%s Record %s is up to date" % (str(user),str(request.remote_addr),fqdn) )
-	cur.close()
-	conn.close()
-		
-
-	return "Up to date",200
 
 
 def init():
-	global opts
-	
-	with open("/etc/pdnsupdater/config.json") as cf:
-		cfg=json.load(cf)
-	
-	lh = logging.getLogger('pdnsupdater')
-	lh.setLevel(logging.WARNING)
+  global cfg
 
-	h = logging.handlers.SysLogHandler(address = '/dev/log')
+  if cfg == None:
+    try:
+      f=open("config.json")
+      cfg=json.load(f)
+    except Exception as e:
+      print("Unable to load configuration file ... %s" % (str(e)))
+      try:
+        f=open("/etc/pdnsupdater/config.json")
+        cfg=json.load(f)
+      except Exception as e:
+        print("Unable to load configuration file ... %s" % (str(e)))
+        sys.exit(-1)
 
-	lh.addHandler(h)
-
-	opts= cfg, lh
-
-	lh.info("pdnsupdater started")
+    f.close()
 
 init()
 
 if __name__ == "__main__":
-	global opts
-        conf, lh = opts
-	app.run(host=conf['listen']['host'],port=int(conf['listen']['port']))
-
-
+  init()
+  app.run(host=cfg['listen']['host'],port=int(cfg['listen']['port']))
